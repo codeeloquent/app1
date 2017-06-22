2b705c24a3387c458723388eae9ba5017d9dda4c
Ryan Eckerle <e_ryan1@yahoo.com>
2017-06-22 16:04:55 -0400

    4.1 generated product scaffolding

---
+++ db/migrate/20170622172009_create_products.rb
@@ -0,0 +1,11 @@
+class CreateProducts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :image_URL
+
+      t.timestamps
+    end
+  end
+end
