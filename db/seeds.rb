b501b9b871c276a8139f303a678b06db84ed4325
Ryan Eckerle <e_ryan1@yahoo.com>
2017-06-28 14:55:08 -0400

    4.2 added color and price field to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create(name: "Tropical Storm", description: "Easily skids through front part
+	of lane with controlled reaction on difficult and dry lane conditions.", 
+	image_URL: "https://images1.bowlingball.com/images/Product/medium/13294.jpg", 
+	colour: "Blue/Oragne", price: "79.99")
