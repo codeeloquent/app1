a8298a71f8cb417dc26f2e0f24f8ced8aeda0a20
Ryan Eckerle <e_ryan1@yahoo.com>
2017-08-07 21:38:03 -0400

    5.3 setting up mailers

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   get 'users/new'
 
   root 'products#index'
+
+  post 'static_pages/thank_you'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
