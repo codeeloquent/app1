d0642344fce6384fd6b47a201e668e6886941e8a
Ryan Eckerle <e_ryan1@yahoo.com>
2017-08-22 00:09:40 -0400

    5.8 implementation of star rating system through comments controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   
-
   devise_for :users, path: '', path_name: { sign_in: 'login', sign_out: 'logout'}
   resources :users
-  resources :products
+  resources :products do
+    resources :comments
+  end
   resources :orders, only: [:index, :show, :create, :destroy]
+  
   get 'static_pages/about'
 
   get 'static_pages/contact'
