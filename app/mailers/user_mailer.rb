b679836a4e84b2686aee470bc4a1bc7abafce14a
Ryan Eckerle <e_ryan1@yahoo.com>
2017-09-17 18:11:11 -0400

    change of email address in user_mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ApplicationMailer
 	def contact_form(email, name, message)
 		@message = message
 			mail(from: email, 
-						to: 'ff.computing@gmail.com',
+						to: 'reckerle00@gmail.com',
 						subject: "A new contact form message from #{name}")
 	end	
 		
