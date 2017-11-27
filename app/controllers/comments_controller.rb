1883a9d04164cc6971de4371a331307c5f96e990
Ryan Eckerle <e_ryan1@yahoo.com>
2017-11-26 19:46:35 -0500

    6.13 alert div fix and variable issue

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
 		@comment.save
 		respond_to do |format|
 			if @comment.save
-				@product.set_recent_reviewer("#{@user.first_name}")
+				@product.set_recent_reviewer("#{current_user.email}")
 				ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
 				format.html {redirect_to @product, notice: 'Review was created successfully.'}
 				format.json {render :show, status: :creatd, location: @product}
